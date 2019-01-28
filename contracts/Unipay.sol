a6ef5aa28d3ae41d8f8a049e93fd4310fc95d906
Vic Woeltjen <vic@thebeetoken.com>
2019-01-28 12:18:11 -0800

    Refactor out Safe libs

--- contracts/Unipay.sol
+++ contracts/Unipay.sol
@@ -3,70 +3,8 @@ pragma solidity ^0.5.2;
 import "openzeppelin-solidity/contracts/token/ERC20/ERC20.sol";
 import "openzeppelin-solidity/contracts/math/SafeMath.sol";
 import 'uniswap-solidity/contracts/Uniswap.sol';
-
-library SafeERC20 {
-    using SafeMath for uint256;
-
-    function transferTokens(
-      ERC20 _token,
-      address _from,
-      address _to,
-      uint256 _value
-    ) internal {
-        uint256 oldBalance = _token.balanceOf(_to);
-        require(
-            _token.transferFrom(_from, _to, _value),
-            "Failed to transfer tokens."
-        );
-        require(
-            _token.balanceOf(_to) >= oldBalance.add(_value),
-            "Balance validation failed after transfer."
-        );
-    }
-
-    function approveTokens(
-      ERC20 _token,
-      address _spender,
-      uint256 _value
-    ) internal {
-        uint256 nextAllowance =
-          _token.allowance(address(this), _spender).add(_value);
-        require(
-            _token.approve(_spender, nextAllowance),
-            "Failed to approve exchange withdrawal of tokens."
-        );
-        require(
-            _token.allowance(address(this), _spender) >= nextAllowance,
-            "Failed to validate token approval."
-        );
-    }
-}
-
-library SafeExchange {
-    using SafeMath for uint256;
-
-    function swapTokens(
-        UniswapExchangeInterface _exchange,
-        uint256 _outValue,
-        uint256 _inValue,
-        uint256 _ethValue,
-        uint256 _deadline,
-        ERC20 _outToken
-    ) internal {
-        uint256 nextBalance = _outToken.balanceOf(address(this)).add(_outValue);
-        _exchange.tokenToTokenSwapOutput(
-            _outValue,
-            _inValue,
-            _ethValue,
-            _deadline,
-            address(_outToken)
-        );
-        require(
-            _outToken.balanceOf(address(this)) >= nextBalance,
-            "Balance validation failed after swap."
-        );
-    }
-}
+import "./safe/SafeERC20.sol";
+import "./safe/SafeExchange.sol";
 
 contract Unipay {
     using SafeMath for uint256;
